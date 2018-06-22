926d7d7ca89a704b15e414ebc8dba80055c6488e
zhaozhe <dgxdks@163.com>
2018-06-22 18:56:05 +0800

    modify

--- Rpc/src/main/java/com/hzgc/compare/rpc/protocol/RpcDecoder.java
+++ Rpc/src/main/java/com/hzgc/compare/rpc/protocol/RpcDecoder.java
@@ -10,10 +10,26 @@ import sun.reflect.Reflection;
 import java.util.List;
 
 public class RpcDecoder extends ByteToMessageDecoder {
+    private Class<?> genericClass;
+
     public RpcDecoder(Class<RpcRequest> rpcRequestClass) {
     }
 
     @Override
-    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+    protected void decode(ChannelHandlerContext channelHandlerContext,
+                          ByteBuf byteBuf,
+                          List<Object> list) throws Exception {
+        if (byteBuf.readableBytes() < 4) {
+            return;
+        }
+        byteBuf.markReaderIndex();
+        int dataLength = internalBuffer().readInt();
+        if (byteBuf.readableBytes() < dataLength) {
+            byteBuf.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        byteBuf.readBytes(data);
+        Object object =
     }
 }
---
+++ Rpc/src/main/java/com/hzgc/compare/rpc/protocol/SerializationUtil.java
@@ -0,0 +1,60 @@
+package com.hzgc.compare.rpc.protocol;
+
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SerializationUtil {
+    private static Map<Class<?>, Schema<?>> classSchema = new ConcurrentHashMap<>();
+    private static Objenesis objenesis = new ObjenesisStd(true);
+
+    private SerializationUtil() {
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> Schema<T> getSchema(Class<T> clz) {
+        Schema<T> schema = (Schema<T>) classSchema.get(clz);
+        if (schema == null) {
+            schema = RuntimeSchema.createFrom(clz);
+            classSchema.put(clz, schema);
+        }
+        return schema;
+    }
+
+    /**
+     *  序列化（对象 -> 字节数组）
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> byte[] serialize(T obj) {
+        Class<T> clz = (Class<T>) obj.getClass();
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        try {
+            Schema<T> schema = getSchema(clz);
+            return ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    /**
+     * 反序列化（字节数组 -> 对象）
+     */
+    public static <T> T deserialize(byte[] data, Class<T> clz) {
+        try {
+            T message = objenesis.newInstance(clz);
+            Schema<T> schema = getSchema(clz);
+            ProtostuffIOUtil.mergeFrom(data, message, schema);
+            return message;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+}
