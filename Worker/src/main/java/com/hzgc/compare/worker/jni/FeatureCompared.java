440700bcd4d5561823561ed063613e2937c521ea
cdb156 <15658067087@163.com>
2018-07-05 18:54:03 +0800

    第二次对比改为批量执行

--- Worker/src/main/java/com/hzgc/compare/worker/jni/FeatureCompared.java
+++ Worker/src/main/java/com/hzgc/compare/worker/jni/FeatureCompared.java
@@ -8,5 +8,5 @@ public class FeatureCompared {
 
     public static native SearchResult compare(List<Pair<String, byte[]>> dataToCompare, byte[] feature, float sim);
 
-    public static native float featureCompare(List<Pair<String, float[]>> data, float[] feature, float sim);
+    public static native SearchResult featureCompare(List<Pair<String, float[]>> data, float[] feature, float sim);
 }
